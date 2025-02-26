6b9f2c6c8c5730a3af33990218b8ac3db542357a
vathana.ph <vathanapheng2@gmail.com>
2025-02-27 00:36:19 +0700

    fixed

--- app/Filament/Resources/UserRegistrationResource/Pages/EditUserRegistration.php
+++ app/Filament/Resources/UserRegistrationResource/Pages/EditUserRegistration.php
@@ -4,16 +4,212 @@ namespace App\Filament\Resources\UserRegistrationResource\Pages;
 
 use App\Filament\Resources\UserRegistrationResource;
 use Filament\Actions;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUserRegistration extends EditRecord
 {
     protected static string $resource = UserRegistrationResource::class;
+    
+    public $role_id;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+                ->schema([
+                    Fieldset::make('User Registration')
+                        ->schema([
+                            Grid::make(4)
+                                ->schema([
+                                    Select::make('role_id')
+                                        ->label('Role')
+                                        ->live(debounce:500)
+                                        ->options(function($record){
+                                            return getRoles();
+                                        })
+                                        ->required()
+                                    ]),
+                            Fieldset::make('Input Information')
+                                ->schema([
+                                    Grid::make(3)
+                                        ->schema([
+                                            TextInput::make('full_name')
+                                                ->label('Full Name')
+                                                ->required()
+                                                ->maxLength(55),
+                                            TextInput::make('email')
+                                                ->label('Email')
+                                                ->email()
+                                                ->required()
+                                                ->unique(ignoreRecord: true)
+                                                ->maxLength(55),
+                                            TextInput::make('password')
+                                                ->label('password')
+                                                ->password()
+                                                ->revealable()
+                                                ->dehydrateStateUsing(fn ($state) => !empty($state) ? bcrypt($state) : null)
+                                                ->dehydrated(fn ($state) => filled($state))
+                                                ->nullable(),
+                                            TextInput::make('phone_number')
+                                                ->label('Phone Number')
+                                                ->rule(['regex:/^\+?[0-9]*$/'])
+                                                ->required()
+                                                ->maxLength(55),
+                                            DatePicker::make('birth')
+                                                ->live(debounce:500)
+                                                ->label('Birth')
+                                                ->native(false)
+                                                ->displayFormat('d F  Y')
+                                                ->required(function($get){
+                                                    $role_id = (int) $get('role_id');
+                                                    if($role_id){
+                                                        $role = getRole($role_id);
+                                                        if(in_array($role?->name, [config('access.role.admin'), config('access.role.teacher')])){
+                                                            return false;
+                                                        }
+                                                        else{
+                                                            return true;
+                                                        }
+                                                    }
+                                                    else{
+                                                        return true;
+                                                    }
+                                                })
+                                                ->hidden(function($get){
+                                                    $role_id = (int) $get('role_id');
+                                                    if($role_id){
+                                                        $role = getRole($role_id);
+                                                        if(in_array($role?->name, [config('access.role.admin'), config('access.role.teacher')])){
+                                                            return true;
+                                                        }
+                                                        else{
+                                                            return false;
+                                                        }
+                                                    }
+                                                    else{
+                                                        return false;
+                                                    }
+                                                }),
+                                            TextInput::make('year')
+                                                ->live(debounce:500)
+                                                ->label('Year')
+                                                ->required(function($get){
+                                                    $role_id = (int) $get('role_id');
+                                                    if($role_id){
+                                                        $role = getRole($role_id);
+                                                        if(in_array($role?->name, [config('access.role.admin'), config('access.role.teacher')])){
+                                                            return false;
+                                                        }
+                                                        else{
+                                                            return true;
+                                                        }
+                                                    }
+                                                    else{
+                                                        return true;
+                                                    }
+                                                })
+                                                ->hidden(function($get){
+                                                    $role_id = (int) $get('role_id');
+                                                    if($role_id){
+                                                        $role = getRole($role_id);
+                                                        if(in_array($role?->name, [config('access.role.admin'), config('access.role.teacher')])){
+                                                            return true;
+                                                        }
+                                                        else{
+                                                            return false;
+                                                        }
+                                                    }
+                                                    else{
+                                                        return false;
+                                                    }
+                                                }),
+                                            TextInput::make('class')
+                                                ->live(debounce:500)
+                                                ->label('Class')
+                                                ->required(function($get){
+                                                    $role_id = (int) $get('role_id');
+                                                    if($role_id){
+                                                        $role = getRole($role_id);
+                                                        if(in_array($role?->name, [config('access.role.admin'), config('access.role.teacher')])){
+                                                            return false;
+                                                        }
+                                                        else{
+                                                            return true;
+                                                        }
+                                                    }
+                                                    else{
+                                                        return true;
+                                                    }
+                                                })
+                                                ->hidden(function($get){
+                                                    $role_id = (int) $get('role_id');
+                                                    if($role_id){
+                                                        $role = getRole($role_id);
+                                                        if(in_array($role?->name, [config('access.role.admin'), config('access.role.teacher')])){
+                                                            return true;
+                                                        }
+                                                        else{
+                                                            return false;
+                                                        }
+                                                    }
+                                                    else{
+                                                        return false;
+                                                    }
+                                                }),
+                                        ])
+                                ]) 
+                                ->live(debounce:500)
+                                ->hidden(function($get){
+                                    $role_id = (int) $get('role_id');
+                                    if($role_id){
+                                        return false;
+                                    }
+                                    return true;
+                                })       
+                        ])
+                        ->columns(1)
+                ]);
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $user = $this->getRecord();
+        $role_id = $user->roles[0]?->id ?? null;
+        if($role_id){
+            $data['role_id'] = $role_id;
+        }
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $this->role_id = (int) $data['role_id'];
+
+        unset($data['role_id']);
+
+        $data['name'] = $data['full_name'];
+
+        return $data;
+    }
+
+    protected function afterSave(): void
+    {
+        $user= $this->getRecord();
+        $role_id = $this->role_id;
+        // assign role to user
+        $user->roles()->sync($role_id);
+        $user->save();
     }
 }
--- app/Filament/Resources/UserRegistrationResource/Pages/ListUserRegistrations.php
+++ app/Filament/Resources/UserRegistrationResource/Pages/ListUserRegistrations.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\UserRegistrationResource\Pages;
 use App\Filament\Resources\UserRegistrationResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\TextInputColumn;
+use Filament\Tables\Table;
 
 class ListUserRegistrations extends ListRecords
 {
@@ -15,5 +18,24 @@ class ListUserRegistrations extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+                ->columns([
+                    TextColumn::make('name')
+                        ->label('Name'),
+                    TextColumn::make('email')
+                        ->label('Email'),
+                    TextColumn::make('roles.name')
+                        ->label('Role'),
+                    TextColumn::make('created_at')
+                        ->label('Created At')
+                        ->dateTime('d F Y'),
+                    TextColumn::make('updated_at')
+                        ->label('Updated At')
+                        ->dateTime('d F Y'),
+                ]);
     }
 }
