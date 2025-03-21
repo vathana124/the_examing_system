6f59ee5c6327761dd8647ac9414295ebc9c7ed7c
vathana.ph <vathanapheng2@gmail.com>
2025-03-22 03:13:56 +0700

    fixed

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,9 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandLogo(asset('assets/images/exam-results.png'))
+            ->brandLogoHeight('3rem')
+            ->favicon(asset('assets/images/exam-results.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
